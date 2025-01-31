d8efe2a53cf5bc845e1c73cc50d6457628a98efd
alexandr <endgage12@gmail.com>
2025-01-31 18:15:33 +0300

    init requests

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
     <TheWelcome />
+
+    <el-button @click="professionCreate">Default</el-button>
   </main>
 </template>
+
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import axios from 'axios'
+
+const professionCreate = async () => {
+  const res = await axios.get('http://localhost:3000/profession/read/3?test=true')
+  console.log(res?.data)
+}
+</script>
