ba07ee14c79825984648c54022609c10e9d83a09
alexandr <endgage12@gmail.com>
2025-01-31 19:54:23 +0300

    init requests

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -11,7 +11,9 @@ import TheWelcome from '../components/TheWelcome.vue'
 import axios from 'axios'
 
 const professionCreate = async () => {
-  const res = await axios.get('http://localhost:3000/profession/read/3?test=true')
+  const res = await axios.post('http://localhost:3000/profession/create?test=true', {
+    data: 'test',
+  })
   console.log(res?.data)
 }
 </script>
