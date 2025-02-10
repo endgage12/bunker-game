b583f1bb6e65da48e00e51db8de6236ae296c5f9
alexandr <endgage12@gmail.com>
2025-02-11 02:04:50 +0300

    frontend socket.service.ts fix

--- frontend/src/services/socket.service.ts
+++ frontend/src/services/socket.service.ts
@@ -7,12 +7,10 @@ import type { Room } from '@/types/roomType.ts'
 import type { Card } from '@/types/cardType.ts'
 
 class SocketService {
-  private socket: Socket
-  private roomList: Array<number> = []
+  private socket: Socket | null
+  private roomList: string[] = []
   private roomStore: ReturnType<typeof useRoomStore> | null = null
   constructor() {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
     this.socket = null
     this.roomList = []
   }
@@ -28,101 +26,60 @@ class SocketService {
       },
     })
 
-    // Обработчики ошибок
-    this.socket.on('connect_error', (err) => {
-      console.error('Connection error:', err)
-    })
-
     this.socket.on('error', (error) => {
       console.error('Socket error:', error)
     })
   }
 
-  // Создание комнаты
   createRoom(playerName: string) {
-    return new Promise((resolve, reject) => {
-      this.socket.emit('createRoom', playerName, async (response: object) => {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
-        if (response.roomId) {
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-expect-error
-          this.roomList = this.roomList ? [...this.roomList, response.roomId] : [response.roomId]
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-expect-error
-          await axios.post('http://localhost:3000/room/create', { roomId: response.roomId })
-          resolve(response)
-        } else {
-          reject(new Error('Failed to create room'))
-        }
-      })
+    if (!this.socket) return
+
+    this.socket.emit('createRoom', playerName, (response: Room) => {
+      this.roomList = this.roomList ? [...this.roomList, response.roomId] : [response.roomId]
     })
+    this.getRooms()
   }
 
-  // Присоединение к комнате
   joinRoom(roomId: string, username: string) {
+    if (!this.socket) return
     this.socket.emit('joinRoom', { roomId, username })
   }
 
   voteForKick(playerId: string) {
+    if (!this.socket) return
     this.socket.emit('voteForKick', { playerId })
-  }
-
-  changePlayerReady(roomId: string, player: object) {
-    return new Promise((resolve, reject) => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      this.socket.emit('changePlayerReady', { roomId, player }, (response) => {
-        console.log(response, !response || Array.isArray(response))
-        if (!response || !Array.isArray(response)) reject(new Error(response.error))
-
-        resolve(response)
-      })
-    })
   }
 
   // Слушатели событий
   onRoomDataUpdated(callback: (room: Room) => void) {
+    if (!this.socket) return
     this.socket.on('onRoomDataUpdated', callback)
   }
 
   onGetRooms(callback: (room: Room[]) => void) {
+    if (!this.socket) return
     this.socket.on('onGetRooms', callback)
   }
 
   onGetMyCard(callback: (cards: Card[]) => void) {
+    if (!this.socket) return
     this.socket.on('onGetMyCard', callback)
   }
 
   // Отправка действий
   startGame() {
+    if (!this.socket) return
     this.socket.emit('startGame')
   }
 
   updateCard(uuid: string, newData: Card) {
+    if (!this.socket) return
     this.socket.emit('updateCard', { uuid, newData })
   }
 
   getRooms() {
+    if (!this.socket) return
     this.socket.emit('getRooms')
-  }
-
-  revealCard(cardType: string) {
-    this.socket.emit('revealCard', {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      roomId: this.roomId,
-      cardType,
-    })
-  }
-
-  submitVote(playerId: string) {
-    this.socket.emit('submitVote', {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      roomId: this.roomId,
-      votedPlayerId: playerId,
-    })
   }
 
   disconnect() {
