541960225d1be6e624de048c43d87993f0411c7c
alexandr <endgage12@gmail.com>
2025-02-11 13:11:54 +0300

    for testing server purposes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+  app.enableCors({ origin: ['http://localhost:3000', 'http://5.35.103.104'] });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
