b8bee08ba7fd6ad03df73994d2c246c1e4068fe2
alexandr <endgage12@gmail.com>
2025-02-15 22:38:00 +0300

    localhost dev fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:3000', 'http://5.35.103.104'] });
+  app.enableCors({ origin: ['http://localhost:8181', 'http://5.35.103.104'] });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
