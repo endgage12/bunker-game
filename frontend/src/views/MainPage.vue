e5efa38fe4002c68a63bb7a876ce2e709814ee56
alexandr <endgage12@gmail.com>
2025-03-11 12:29:46 +0300

    for test purposes

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -60,7 +60,7 @@ const goToRoomList = () => {
 }
 
 onBeforeMount(async () => {
-  console.log(`'v0.2' ${apiURL}`)
+  console.log(`'v0.3' ${apiURL}`)
   menu.value = (await axios.get(`${apiURL}/setting`)).data
 })
 </script>
