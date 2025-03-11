63ca5da5bc963e0934620f6a87828f39e357527a
alexandr <endgage12@gmail.com>
2025-03-11 11:58:59 +0300

    for test purposes

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService, Card } from './app.service';
 import { Setting } from './modules/settings/entities/setting.entity';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  sayHello(): string {
+    return 'hi';
+  }
 
   @Post('/card/generate')
   async cardGenerate(@Body() params: object): Promise<Card> {
